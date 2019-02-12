6b98586487da39559836856c57815abf0b158a6c
Filipe Cordeiro <filipe.rolim@gmail.com>
2019-02-12 13:11:49 -0300

    add update8

--- codes/aula3/kt_utils.py
+++ codes/aula3/kt_utils.py
@@ -9,11 +9,11 @@ def mean_pred(y_true, y_pred):
     return K.mean(y_pred)
 
 def load_dataset():
-    train_dataset = h5py.File('datasets/train_happy.h5', "r")
+    train_dataset = h5py.File('cursoDL/codes/aula3/train_happy.h5', "r")
     train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
     train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
 
-    test_dataset = h5py.File('datasets/test_happy.h5', "r")
+    test_dataset = h5py.File('cursoDL/codes/aula3/test_happy.h5', "r")
     test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
     test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
 

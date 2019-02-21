897df4fdff738c80460cd45f392261e78744e9bb
Filipe Cordeiro <filipe.rolim@gmail.com>
2019-02-20 21:50:31 -0300

    add update28

--- codes/aula5/nn/conv/__init__.py
+++ codes/aula5/nn/conv/__init__.py
@@ -3,3 +3,6 @@ from .shallownet import ShallowNet
 from .lenet import LeNet
 from .minivggnet import MiniVGGNet
 from .fcheadnet import FCHeadNet
+from .alexnet import AlexNet
+from .minigooglenet import MiniGoogLeNet
+from .deepergooglenet import DeeperGoogLeNet
--- codes/aula5/preprocessing/__init__.py
+++ codes/aula5/preprocessing/__init__.py
@@ -2,3 +2,6 @@
 from .simplepreprocessor import SimplePreprocessor
 from .aspectawarepreprocessor import AspectAwarePreprocessor
 from .imagetoarraypreprocessor import ImageToArrayPreprocessor
+from .meanpreprocessor import MeanPreprocessor
+from .croppreprocessor import CropPreprocessor
+from .patchpreprocessor import PatchPreprocessor
